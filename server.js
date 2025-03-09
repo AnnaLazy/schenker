b401aabf66b0123bf765215e0ade556afddd5d43
AnnaLazy <65811252+AnnaLazy@users.noreply.github.com>
2025-03-09 20:05:45 +0100

    new

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" })); // Увеличиваем лимит на JSON (изображения могут быть большими)
 app.use(fileUpload()); // Подключаем middleware для загрузки файлов
 
+app.get("/", (req, res) => {
+  res.send("Сервер работает!");
+});
+
 // Подключение к базе данных
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
