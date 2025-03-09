4d0c3f03798e2a91769c64794f56f8ce75ad46a3
AnnaLazy <65811252+AnnaLazy@users.noreply.github.com>
2025-03-09 21:01:06 +0100

    new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Указываем относительный путь для корректной работы
+  build: {
+    outDir: "dist" // Vercel ожидает, что билды находятся в dist
+  }
 })
